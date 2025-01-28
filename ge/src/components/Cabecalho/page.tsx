b228c08386199e83de3b798be97348635f8bd745
gusandrini <gustavosandrini92@gmail.com>
2025-01-28 01:17:11 -0300

    criado estrutura base cabeçalho

--- ge/src/components/Cabecalho/page.tsx
+++ ge/src/components/Cabecalho/page.tsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Cabecalho = ()  => {
   return (
-    <div>Cabecalho</div>
+    <header>
+        <div>
+            <div className="foto_menu">
+                <Link href="/">
+                    <Image
+                    src={}
+                    alt=''
+                    />
+                </Link>
+            </div>
+        </div>
+
+        <div className="container-cabecalho">
+            <div className="menu">
+                <nav>
+                    <ul>
+                        <li><Link href="/sobre" className="link">SOBRE NÓS</Link></li>
+                        <li><Link href="/blog" className="link">BLOG</Link></li>
+                        <li><Link href="/contato" className="link">CONTATO</Link></li>
+                        <li><Link href="/produtos" className="link">PRODUTOS</Link></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
+    
   )
 }
 export default Cabecalho;
